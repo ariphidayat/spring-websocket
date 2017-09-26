786a85fd3e29925ee7604bae7b34d460f3c6a3ae
Arip Hidayat <ariphidayat@hotmail.com>
2017-09-26 21:47:54 +0700

    WebSocket Security

---
+++ src/main/java/com/arip/websocket/WebSocketSecurityConfig.java
@@ -0,0 +1,22 @@
+package com.arip.websocket;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
+import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
+
+/**
+ * Created by Arip Hidayat on 9/26/2017.
+ */
+@Configuration
+public class WebSocketSecurityConfig extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages.anyMessage().authenticated();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
+}
