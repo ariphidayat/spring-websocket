7444250039c3707c04294963ca95087d817f19b6
Arip Hidayat <ariphidayat@hotmail.com>
2017-10-08 20:01:06 +0700

    MessageExceptionHandler

--- src/main/java/com/arip/websocket/MessageController.java
+++ src/main/java/com/arip/websocket/MessageController.java
@@ -1,10 +1,9 @@
 package com.arip.websocket;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.messaging.handler.annotation.DestinationVariable;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.*;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
@@ -20,11 +19,21 @@ public class MessageController {
 
     @MessageMapping("/chat/{to}")
     public void onMessage(@Payload Message msg, @DestinationVariable("to") String to, Principal principal) {
+        if (msg.getContent().length() < 1) {
+            throw new IllegalArgumentException("Content shouldn't be empty.");
+        }
+
         msg.setFrom(principal.getName());
         if (to.equals("public")) {
             simpMessagingTemplate.convertAndSend("/topic/messages", msg);
         }else{
             simpMessagingTemplate.convertAndSendToUser(to, "/queue/messages", msg);
         }
+    }
+
+    @MessageExceptionHandler
+    @SendToUser(value = "/queue/messages", broadcast = false)
+    public Message handleException(IllegalArgumentException e) {
+        return new Message("ADMIN", e.getMessage());
     }
 }
