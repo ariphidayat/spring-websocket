863f82946bf240de2f753fb865fb8cdab370da40
Arip Hidayat <ariphidayat@hotmail.com>
2017-11-17 13:01:49 +0700

    add session connect/disconnect listener

---
+++ src/main/java/com/arip/websocket/SessionDisconnectEventHandler.java
@@ -0,0 +1,22 @@
+package com.arip.websocket;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+
+/**
+ * Created by Arip Hidayat on 11/17/2017.
+ */
+@Component
+public class SessionDisconnectEventHandler implements ApplicationListener<SessionDisconnectEvent> {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+    @Override
+    public void onApplicationEvent(SessionDisconnectEvent e) {
+        simpMessagingTemplate.convertAndSend("/topic/messages", new Message("system", e.getUser().getName() + " disconnected!"));
+    }
+}
